933a038c96b4f4826df639dd04b6c6282304bbaa
shaqdeff <shaquillendunda@gmail.com>
2023-05-16 12:12:50 +0300

    Delete content in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fab);
 
 const App = () => {
-  return <div className="text-red-500">Hey you</div>;
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
 };
 
 export default App;
