6c1c51f32b382351333c16425158f04f8aa77576
shaqdeff <shaquillendunda@gmail.com>
2023-05-16 11:59:56 +0300

    Import font awesome library into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+
 const App = () => {
   return <div className="text-red-500">Hey you</div>;
 };
